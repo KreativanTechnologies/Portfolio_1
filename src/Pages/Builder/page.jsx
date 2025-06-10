1968fdb8955b34acdb9411bb8c5f169af4005ac3
Rahul Yadav <rahulyad8860@gmail.com>
2025-06-10 15:27:43 +0530

    ''

--- src/Pages/Builder/page.jsx
+++ src/Pages/Builder/page.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar.jsx'
+import Footer from '../../components/Footer.jsx'
+import Hero from '../../components/Ind/Hero.jsx'
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <Navbar />
+
+      <div>
+        <Hero />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
   )
 }
 
